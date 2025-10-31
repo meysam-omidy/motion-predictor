0a3a356891182bb4fa71ff2f1df4d9e4216d6c3f
alex <alexford@gmail.com>
2025-10-31 12:43:31 +0330

    new structure of codes

---
+++ dataset.py
@@ -0,0 +1,96 @@
+import os
+import torch
+from torch.utils.data import Dataset
+import pandas as pd
+import numpy as np
+import random
+import configparser
+
+def has_jump(seq):
+    return not ((seq[-1]- seq[0] + 1) == len(seq))
+
+class GTSequenceDataset(Dataset):
+
+    @staticmethod
+    def load_sequence(seq_path, seq_in_len, seq_out_len, seq_total_len, random_jump, noise):
+        sources = []
+        targets = []
+
+        gt_path = os.path.join(seq_path, 'gt', 'gt.txt')
+        if not os.path.exists(gt_path):
+            return [], []
+        cfp = configparser.ConfigParser()
+        cfp.read(os.path.join(seq_path, 'seqinfo.ini'))
+        image_width = np.array(cfp['Sequence']['imWidth']).astype(float)
+        image_height = np.array(cfp['Sequence']['imHeight']).astype(float)
+
+        df = pd.read_csv(gt_path, header=None)
+        df.columns = ['frame', 'id', 'x', 'y', 'w', 'h', 'conf', 'class', 'visibility']
+
+        for obj_id, obj_df in df.groupby('id'):
+            obj_df = obj_df.sort_values('frame')
+            obj_df['w'] += obj_df['x']
+            obj_df['h'] += obj_df['y']
+            bboxes = obj_df[['x', 'y', 'w', 'h']].to_numpy().astype(float)
+            borders = np.array([image_width, image_height, image_width, image_height]).astype(float)
+            if noise:
+                bboxes += np.random.randn(bboxes.shape[0], bboxes.shape[1]) * 0.02 * borders
+            bboxes /= borders
+            frames_total = obj_df['frame'].to_numpy()
+            # create sequences of length seq_len
+            for i in range(len(bboxes) - seq_total_len): 
+                seq = bboxes[i:i+seq_total_len]
+                frames = frames_total[i:i+seq_total_len]
+                    
+                if not random_jump:
+                    if has_jump(frames[:seq_in_len]) or has_jump(frames[-seq_out_len:]):
+                        continue
+                    sources.append(seq[:seq_in_len])
+                    targets.append(seq[-seq_out_len:])
+                else:
+                    index_1 = random.randint(0, int(seq_total_len / 2) - seq_in_len - 1)
+                    index_2 = random.randint(0, int(seq_total_len / 2) - seq_in_len - 1)
+                    if has_jump(frames[index_1: index_1 + seq_in_len]) or \
+                    has_jump(frames[int(seq_total_len / 2) + index_2: int(seq_total_len / 2) + index_2 + seq_out_len:]):
+                        continue
+                    sources.append(seq[index_1: index_1 + seq_in_len])
+                    targets.append(seq[int(seq_total_len / 2) + index_2: int(seq_total_len / 2) + index_2 + seq_out_len:])
+
+        return sources, targets, (image_width, image_height)
+
+
+    @classmethod
+    def from_sequence(cls, seq_path, seq_in_len=20, seq_out_len=10, seq_total_len=20, random_jump=False, noise=True):
+        obj = cls()
+        sources, targets, (image_width, image_height) = cls.load_sequence(seq_path, seq_in_len, seq_out_len, seq_total_len, random_jump, noise)
+        obj.sources = np.array(sources, dtype=np.float32)
+        obj.targets = np.array(targets, dtype=np.float32)
+        obj.image_width = image_width
+        obj.image_height =image_height
+        return obj
+    
+
+    @classmethod
+    def from_roots(cls, root_dirs, seq_in_len=20, seq_out_len=10, seq_total_len=20, random_jump=False, noise=True):
+        sources = []
+        targets = []
+
+        for root in root_dirs:
+            sequences = [os.path.join(root, d) for d in os.listdir(root) if os.path.isdir(os.path.join(root, d))]
+            for seq_path in sequences:
+                sources_, targets_, _ = cls.load_sequence(seq_path, seq_in_len, seq_out_len, seq_total_len, random_jump, noise)
+                sources.extend(sources_)
+                targets.extend(targets_)
+
+        obj = cls()
+        obj.sources = np.array(sources, dtype=np.float32)
+        obj.targets = np.array(targets, dtype=np.float32)
+        return obj
+
+    def __len__(self):
+        return len(self.sources)
+
+    def __getitem__(self, idx):
+        source = self.sources[idx]
+        target = self.targets[idx]
+        return torch.tensor(source), torch.tensor(target)
---
+++ loss.py
@@ -0,0 +1,23 @@
+import torch
+from torch import nn
+
+class LossFunction(nn.Module):
+    def forward(self, targets, preds):
+        bb1 = targets.unsqueeze(2)        # (B, N, 1, 4)
+        bb2 = preds.unsqueeze(1)      # (B, 1, N, 4)
+        xx1 = torch.maximum(bb1[..., 0], bb2[..., 0])
+        yy1 = torch.maximum(bb1[..., 1], bb2[..., 1])
+        xx2 = torch.minimum(bb1[..., 2], bb2[..., 2])
+        yy2 = torch.minimum(bb1[..., 3], bb2[..., 3])
+        w = torch.clamp(xx2 - xx1, min=0)
+        h = torch.clamp(yy2 - yy1, min=0)
+        inter = w * h
+        area1 = (bb1[..., 2] - bb1[..., 0]) * (bb1[..., 3] - bb1[..., 1])
+        area2 = (bb2[..., 2] - bb2[..., 0]) * (bb2[..., 3] - bb2[..., 1])
+        union = area1 + area2 - inter
+        ious = inter / union
+        ious = torch.diagonal(ious, dim1=1, dim2=2)
+        loss1 = nn.functional.l1_loss(ious, torch.ones_like(ious))
+        loss2 = nn.functional.smooth_l1_loss(targets, preds)
+        # return loss1
+        return loss1 + loss2
